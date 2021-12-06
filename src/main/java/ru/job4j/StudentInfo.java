6ef390b11ed12eb1cc21a0b9c545842ce6eb7b97
Anastasia <58229482+applejesos@users.noreply.github.com>
2021-12-06 23:56:03 +0400

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nana Atlanova");
+        System.out.println("11.04.1999");
+    }
+}
