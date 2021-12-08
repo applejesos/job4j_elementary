fb604493c121276b609ecf1e25c78b936e278ff8
Anastasia <58229482+applejesos@users.noreply.github.com>
2021-12-09 00:50:18 +0400

    1.2. Входные данные в тесте [#297750]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,6 +1,7 @@
-package ru.job4j.converter;
+package ru.job4j.calculator;
 
 public class Fit {
+
     public static double manWeight(short mHeight) {
         return (mHeight - 100) * 1.15;
     }
@@ -10,11 +11,12 @@ public class Fit {
     }
 
     public static void main(String[] args) {
-        short mHeight = 187;
-        short wHeight = 167;
+        short mHeight = 180;
+        short wHeight = 170;
         double man = Fit.manWeight(mHeight);
         double woman = Fit.womanWeight(wHeight);
         System.out.println("Man " + mHeight + " is " + man);
         System.out.println("Woman " + wHeight + " is " + woman);
     }
+
 }
