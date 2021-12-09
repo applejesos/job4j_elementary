9b935fa346da24b5b92f08069e1302bcd8f744ab
Anastasia <58229482+applejesos@users.noreply.github.com>
2021-12-09 17:10:17 +0400

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -18,31 +18,33 @@ public class Converter {
     }
 
     public static void main(String[] args) {
+        float in = 140;
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2 euro. Test result : " + passed);
+
         float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + dollar + " dollars.");
+        expected = 2.3333333f;
+        out = Converter.rubleToDollar(in);
+        boolean passed1 = expected == out;
+        System.out.println("140 rubles are about 2 dollars. Test result : " + passed1);
+
         float dtr = Converter.dollarToRuble(140);
         System.out.println("140 dollars are " + dtr + " rubles.");
+        expected = 8400;
+        out = Converter.dollarToRuble(in);
+        boolean passed2 = expected == out;
+        System.out.println("140 dollars are 8400 rubles. Test result : " + passed2);
+
         float etr = Converter.euroToRubles(140);
         System.out.println("140 euro are " + etr + " rubles.");
-
-        float in = 140;
-        float rToEexpected = 2;
-        float rToDexpected = 2.3333333f;
-        float dToRexpected = 8400;
-        float eToRexpected = 9800;
-        float out = Converter.rubleToEuro(in);
-        float out1 = Converter.rubleToDollar(in);
-        float out2 = Converter.dollarToRuble(in);
-        float out3 = Converter.euroToRubles(in);
-        boolean passed = rToEexpected == out;
-        System.out.println("140 rubles are 2 euro. Test result : " + passed);
-        boolean passed1 = rToDexpected == out1;
-        System.out.println("140 rubles are about 2 dollars. Test result : " + passed1);
-        boolean passed2 = dToRexpected == out2;
-        System.out.println("140 dollars are 8400 rubles. Test result : " + passed2);
-        boolean passed3 = eToRexpected == out3;
+        expected = 9800;
+        out = Converter.euroToRubles(in);
+        boolean passed3 = expected == out;
         System.out.println("140 euro are 9800 rubles. Test result : " + passed3);
     }
 }
